f5aecf6bc4ee3acbece64a395e14b2dca7572b1f
0nmyhead <50978235+0nmyhead@users.noreply.github.com>
2019-05-28 23:04:31 +0900

    Add files via upload

---
+++ WPG/Watics/Classes/ObWatics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watics.Classes
+{
+    public class ObWatics
+    {
+        private string name;
+
+        public string NameProperty
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+    }
+}
